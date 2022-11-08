a3a26ba04e67463425996de5b059da8558a8536b
sohammukher <sohammukherjee1996@gmail.com>
2022-11-08 14:57:22 -0500

    Test Case to check if the index.html file exists in the given path

--- employee_mgmt_system_proj/emp_app/tests.py
+++ employee_mgmt_system_proj/emp_app/tests.py
@@ -1,3 +1,15 @@
+from os import getcwd
+from os.path import isfile
+
 from django.test import TestCase
+from django.test import SimpleTestCase
+
 
 # Create your tests here.
+
+# testing index function in views.py
+
+class test_functions(SimpleTestCase):
+    #Testing if Index.html Exists
+    def test_index(self):
+        assert isfile('emp_app/templates/index.html')
