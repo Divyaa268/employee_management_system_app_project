95825541eea988e88c2fbc25494c4a030318ca16
sohammukher <sohammukherjee1996@gmail.com>
2022-11-08 20:17:24 -0500

    Made test Case to test add_emp module

--- employee_mgmt_system_proj/emp_app/tests.py
+++ employee_mgmt_system_proj/emp_app/tests.py
@@ -10,6 +10,22 @@ from django.test import SimpleTestCase
 # testing index function in views.py
 
 class test_functions(SimpleTestCase):
-    #Testing if Index.html Exists
+    # Testing if Index.html Exists
     def test_index(self):
+        file_name = 'emp_app/templates/index.html'
+        if not isfile(file_name):
+            print("File " + file_name + " Does Not Exist, Test Case Failed")
+        else:
+            print("File Exists Test Case is Successful")
+
+        assert isfile('emp_app/templates/index.html')
+
+    def test_add_emp(self):
+        #Checks if the page to add employee exists
+        file_name = 'emp_app/templates/index.html'
+        if not isfile(file_name):
+            print("File " + file_name + " Does Not Exist, Test Case Failed")
+        else:
+            print("File Exists Test Case is Successful")
+
         assert isfile('emp_app/templates/index.html')
