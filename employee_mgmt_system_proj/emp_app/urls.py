b598c38e04099b38dbade013820ef2db60130144
sohammukher <sohammukherjee1996@gmail.com>
2022-11-12 14:17:02 -0500

    Revert "new build"
    This reverts commit 43f4bbe706c0691bb72116d94028ebbd380b0d19.

--- employee_mgmt_system_proj/emp_app/urls.py
+++ employee_mgmt_system_proj/emp_app/urls.py
@@ -28,7 +28,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),  # Routing to index page, sits at the root link
     path('all_emp', views.all_emp, name='all_emp'),
-    path('add_emp', views.add_emp, name='add_emp'),#Route to add_emp page, to add new employee data
+    path('add_emp', views.add_emp, name='add_emp'),#Routes to add_emp page, to add new employee data
     path('remove_emp', views.remove_emp, name='remove_emp'),
     path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
     path('filter_emp', views.filter_emp, name='filter_emp'),
